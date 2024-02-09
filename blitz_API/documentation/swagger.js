4373523067c5485cc781ad35a329d15384083e89
Juan Pablo Parra Rivillas <49217502+Darkron98@users.noreply.github.com>
2024-02-09 12:14:41 -0500

    update swagger description

--- blitz_API/documentation/swagger.js
+++ blitz_API/documentation/swagger.js
@@ -7,16 +7,17 @@ const options = {
     info: {
       title: 'BLITZ APP API',
       version: '1.0.0',
+      description: "## Welcome!\n\nThis is **BLITZ APP**, an academic project presented as a requirement for the degree of system engineering career at Universidad del valle; it consists of a mobile app to manage restaurant home deliveries.\n\n **Author:** *Juan Pablo Parra Rivillas*\n\n**Institution:** Universidad del Valle\n\n**Student code:** 1765532\n\n**career:** 3743 - Systems Engineering\n\n**Contact:** juan.pablo.parra@correounivalle.edu.co",
     },
     servers: [
-      {
-        url: "http://localhost:8080/api/v1",
-        description: "Local server"
-      },
       {
         url: "https://blitz-api-dev.fly.dev/api/v1",
         description: "Host server"
       },
+      {
+        url: "http://localhost:8080/api/v1",
+        description: "Local server"
+      }
     ]
   },
   // looks for configuration in specified directories
